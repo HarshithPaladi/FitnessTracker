33d86eb1f262968cd4d9b77342ce3256bfd66acf
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-05-07 02:14:25 +0530

    STarted dark mode toggle

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,13 +10,46 @@ $('#Type').on('change', function () {
         $('#custom-type').addClass('d-none');
     }
 });
+// [Todo] - Refactor this code to toggle the dark theme
+// const toggleBtn = document.getElementById('theme-toggle');
+// const body = document.querySelector('body');
+
+// toggleBtn.addEventListener('click', function () {
+//     body.classList.toggle('dark');
+//     const navbar = document.querySelector('.navbar');
+//     navbar.classList.toggle('dark');
+//     const table = document.querySelector('table');
+//     table.classList.toggle('dark');
+//     const body = document.querySelector('body');
+//     body.classList.toggle('dark');
+//     const btnPrimary = document.querySelectorAll('.btn-primary');
+//     btnPrimary.forEach(function (btn) {
+//         btn.classList.toggle('dark');
+//     });
+
+//     const card = document.querySelectorAll('.card');
+//     card.forEach(function (card) {
+//         card.classList.toggle('dark');
+//     });
+
+//     const cardTitle = document.querySelectorAll('.card-title');
+//     cardTitle.forEach(function (title) {
+//         title.classList.toggle('dark');
+//     });
+
+//     const cardText = document.querySelectorAll('.card-text');
+//     cardText.forEach(function (text) {
+//         text.classList.toggle('dark');
+//     });
+// });
+
 
 $(document).ready(function () {
     $("#workoutSearchInput").on("keyup", function () {
         var value = $(this).val().toLowerCase().trim();
         if (value) {
             $.ajax({
-                url: "/Workouts/SearchWorkouts?searchString=" + value,
+                url: "/api/WorkoutSearchAPI?searchString=" + value,
                 type: "GET",
                 success: function (response) {
                     $("#workoutDropdownMenu").html(response);
